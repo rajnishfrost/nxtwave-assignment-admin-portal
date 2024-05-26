ba28eeb4b09fb4cae56f0029176820b44dcf696c
rajnishfrost <rajnishfrost@gmail.com>
2024-05-26 19:45:18 +0530

    login image url change

--- src/Pages/UserCredential/Login.jsx
+++ src/Pages/UserCredential/Login.jsx
@@ -1,22 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux';
 import "./login.css";
-import {login} from "../../Redux/authSlice"
+import { login } from "../../Redux/authSlice"
+import axios from 'axios';
 
 export default function Login() {
   const [input, setInput] = useState({});
   // eslint-disable-next-line
   const [error, setError] = useState("");
   const dispatch = useDispatch();
+  const [image, setImage] = useState("")
 
   const inputChange = (e) => {
     const { name, value } = e.target;
     setInput({ ...input, [name]: value })
   }
 
+  useEffect(() => {
+    async function getImage() {
+      const data = await axios.get("https://atomic-swap-backend-blockchain.onrender.com/api/v1/user/image-upload-link");
+      setImage(data?.data);
+    }
+    getImage();
+    // eslint-disable-next-line
+  }, [])
+
+
   return (
     <div>
-      <div className='d-flex justify-center' style={{ backgroundSize: "cover", backgroundPosition: "center", backgroundImage: `url(https://images.pexels.com/photos/1402787/pexels-photo-1402787.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`, alignItems: "center", height: "100vh" }}>
+      <div className='d-flex justify-center' style={{ backgroundSize: "cover", backgroundPosition: "center", backgroundImage: `url(${image})`, alignItems: "center", height: "100vh" }}>
         <div>
           <input min="10" max="10" className='d-block mt-10' type="number" placeholder='Phone Number' name="phone" onChange={(e) => inputChange(e)} style={{ width: "300px", height: "30px", padding: "3px" }} />
           <input className='mt-10 d-block' type="password" placeholder='Password' name="password" onChange={(e) => inputChange(e)} style={{ width: "300px", height: "30px", padding: "3px" }} />
